9126680e6868cd36c8cb6465dd80ebb04d62c1c5
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-13 17:41:31 +0900

    chore: add license comment

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,7 @@
+/**
+ * Most of code is taken from https://github.com/vueuse/vueuse
+ * Copyright (c) 2019-PRESENT Anthony Fu<https://github.com/antfu>
+ */
+
 export { useLocalStorageValue } from './use-local-storage-value';
 export { useEventListener } from './use-event-listener';
