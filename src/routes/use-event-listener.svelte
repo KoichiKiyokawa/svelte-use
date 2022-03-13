4888109df299d77ae063668e498947c7b8f1e808
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-13 16:33:34 +0900

    test: `useEventListener`

---
+++ src/routes/use-event-listener.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { useEventListener } from '$lib';
+
+	useEventListener('keydown', (e) => {
+		console.log(e);
+	});
+
+	useEventListener(null, 'keydown', (e) => {
+		console.log(e);
+	});
+</script>
